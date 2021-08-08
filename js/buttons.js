4466b44517230760c0c046ddad9341a7cf3a0ce7
rickNoise <emberix@gmail.com>
2021-08-08 15:32:19 +1000

    Added some buttons in the index.html with accompanying JS script to test different ways of implementing event listeners.

---
+++ js/buttons.js
@@ -0,0 +1,9 @@
+// code for method 2
+const btn = document.querySelector("#btn");
+btn.onclick = () => alert("Hello World!");
+
+// code for method 3
+const btn2 = document.querySelector("#btn2");
+btn2.addEventListener("click", () => {
+  alert("Hello World");
+});
